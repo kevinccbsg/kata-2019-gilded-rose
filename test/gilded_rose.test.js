164a3b23138e02a2616af3825d99817a3b5794d2
kevin Martínez <kevinccbsg@gmail.com>
2019-11-29 16:00:34 +0100

    Test sellIn property

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,15 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const {Shop, Item} = require('../src/gilded_rose');
 
-describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+describe('Gilded Rose', function() {
+  it('should foo', function() {
+    const gildedRose = new Shop([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe('foo');
+  });
+
+  it(`items should have a SellIn value which denotes
+      the number of days we have to sell the item`, () => {
+    const item = new Item('foo', 0, 0);
+    expect(item).toHaveProperty('sellIn');
   });
 });
