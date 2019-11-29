fa28920e17b69305a40ba6f98db3e7f39fd7f832
kevin Martínez <kevinccbsg@gmail.com>
2019-11-29 17:19:47 +0100

    Refactor gilded_rose update quality method

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -9,54 +9,82 @@ class Item {
 class Shop {
   constructor(items=[]){
     this.items = items;
+    this.exceptions = [
+      'Backstage passes to a TAFKAL80ETC concert',
+      'Sulfuras, Hand of Ragnaros',
+      'Aged Brie',
+      'Conjured',
+    ];
+    this.qualityOperator = 1;
+    this.sellInOperator = 1;
+    this.qualityLimit = 50;
   }
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
 
+  expiredItem(item) {
+    if (item.sellIn <= 0) return true;
+    return false
+  }
+
+  setQualityOperator(value) {
+    this.qualityOperator = value;
+  }
+
+  commonItem(item) {
+    if (this.exceptions.includes(item.name)) return item;
+    if (this.expiredItem(item)) this.setQualityOperator(this.qualityOperator * 2);
+    if (item.quality === 0) this.setQualityOperator(0);
+    return {
+      ...item,
+      quality: item.quality - this.qualityOperator,
+      sellIn: item.sellIn - this.sellInOperator,
+    };
+  }
+
+  agedBrie(item) {
+    if (this.exceptions[2] !== item.name) return item;
+    if (item.quality === this.qualityLimit) this.setQualityOperator(0);
+    return {
+      ...item,
+      quality: item.quality + this.qualityOperator,
+      sellIn: item.sellIn - this.sellInOperator,
+    };
+  }
+
+  backstage(item) {
+    if (this.exceptions[0] !== item.name) return item;
+    if (item.sellIn === 0) return { ...item, quality: 0 };
+    if (item.sellIn <= 10) this.setQualityOperator(2);
+    if (item.sellIn <= 5) this.setQualityOperator(3);
+    return {
+      ...item,
+      quality: item.quality + this.qualityOperator,
+      sellIn: item.sellIn - this.sellInOperator,
+    };
+  }
+
+  conjured(item) {
+    if (this.exceptions[3] !== item.name) return item;
+    this.setQualityOperator(2);
+    return {
+      ...item,
+      quality: item.quality - this.qualityOperator,
+      sellIn: item.sellIn - this.sellInOperator,
+    };
+  }
+
+  updateQuality() {
+    this.items = this.items.map(function (item) {
+      return this.commonItem(item);
+    }, this)
+    .map(function (item) {
+      return this.agedBrie(item);
+    }, this)
+    .map(function (item) {
+      return this.backstage(item);
+    }, this)
+    .map(function (item) {
+      return this.conjured(item);
+    }, this);
     return this.items;
   }
 }
